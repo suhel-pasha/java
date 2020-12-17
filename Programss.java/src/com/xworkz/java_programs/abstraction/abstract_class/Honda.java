248fd19b3fa3965e2974ace28f2e0bc208634352
suhel-pasha <74663794+suhel-pasha@users.noreply.github.com>
2020-12-17 11:39:17 +0530

    Update Honda.java

--- Programss.java/src/com/xworkz/java_programs/abstraction/abstract_class/Honda.java
+++ Programss.java/src/com/xworkz/java_programs/abstraction/abstract_class/Honda.java
@@ -2,7 +2,7 @@ package com.xworkz.java_programs.abstraction.abstract_class;
 
 abstract public class Honda {
 	void engine() {
-		System.out.println("Honda assembles Engine");
+		System.out.println("Honda assembless Engine");
 	}
 	abstract void body();
 
